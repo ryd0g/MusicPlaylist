c25c0f652458c5d00b8b5b16f7d68e8183ec11d5
Ryan Lee <rjlguitar@gmail.com>
2021-01-27 16:22:21 -0800

    added methods for Song

--- Song.py
+++ Song.py
@@ -7,29 +7,30 @@ class Song:
 
   # TODO: Create a getter method for the title attribute, called get_title
   def get_title(self):
-    pass
+    return self.__title
   
   
   # TODO: Create a setter method for the next_song attribute, called set_title. Make sure titles are type cased to strings and are Title Cased.
   def set_title(self, title):
-    pass
+    self.__title = title
 
 
   # TODO: Create a getter method for the next_song attribute, called get_next_song
   def get_next_song(self):
-    pass
+    return self.__next_song
 
 
   # TODO: Create a setter method for the next_song attribute, called set_next_song
   def set_next_song(self, next_title):
-    pass
+    self.__next_song = next_title
 
 
   # TODO: Using the __str___ dunder method, return a string of the song title.
   def __str__(self):
-    pass
+    return str(self.__title)
 
 
   # TODO: Using the __repr__ dunder method, return a string formatted as the following:'Song Title -> Next Song Title'
   def __repr__(self):
-    pass
+    print('Song Title --> Next Song Title')
+    
